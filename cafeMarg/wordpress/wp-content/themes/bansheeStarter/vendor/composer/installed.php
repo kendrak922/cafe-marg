48f12efa9de51ce078aca84c3732168a7b9c31cd
Kendra Kyndberg <kendrak922@gmail.com>
2025-07-17 13:32:31 -0500

    initial push

---
+++ cafeMarg/wordpress/wp-content/themes/bansheeStarter/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'fc9a87134583ae870571fb3de5ac4cb7c3cd7bf0',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'fc9a87134583ae870571fb3de5ac4cb7c3cd7bf0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'moxie-lean/loader' => array(
+            'pretty_version' => '1.3.2',
+            'version' => '1.3.2.0',
+            'reference' => 'cc82e3cf004516e7402dea0ac39bcb03e175b5f3',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../moxie-lean/loader',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
