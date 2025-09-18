31c3c18c664afc8a777dfbee22fe8390384917a6
Kendra Kyndberg <kendrak922@gmail.com>
2025-09-18 11:14:43 -0500

    allow container in page banner

--- wordpress/wp-content/themes/bansheeStarter/template-parts/blocks/page-banner/page-banner.php
+++ wordpress/wp-content/themes/bansheeStarter/template-parts/blocks/page-banner/page-banner.php
@@ -17,7 +17,8 @@ $blocks_allowed = array(
     'core/paragraph',
     'yoast-seo/breadcrumbs',
     'acf/image',
-    'acf/spacer'
+    'acf/spacer',
+    'acf/container'
 );
 $blocks_template = array(
     array('core/heading', array()),
